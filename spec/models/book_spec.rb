2710c3c60a368800fbce104868e23b9bda9c38dd
KouOsada <kouosada.0930@gmail.com>
2022-01-11 13:01:10 +0000

    [Fix]book_spec.rb

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -2,61 +2,61 @@
 
 require 'rails_helper'
 
-RSpec.describe Book, "モデルに関するテスト", type: :model do
-  describe '実際に保存してみる' do
-    it "有効な投稿内容の場合は保存されるか" do
-      expect(FactoryBot.build(:book)).to be_valid
-    end
-  end
-  context "空白のバリデーションチェック" do
-    it "titleが空白の場合にバリデーションチェックされ空白のエラーメッセージがが返ってきているか" do
-      book = Book.new(title: '', body: 'hoge')
-      expect(book).to be_invalid
-      expect(book.errors[:title]).to include("can't be blank")
-    end
-    it "bodyが空白の場合にバリデーションチェックされ空白のエラーメッセージがが返ってきているか" do
-      book = Book.new(title: 'hoge', body: '')
-      expect(book).to be_invalid
-      expect(book.errors[:body]).to include("can't be blank")
-    end
-  end
-end
-
-# Rspec.describe 'Bookモデルのテスト', type: :model do
-#   describe 'バリデーションのテスト' do
-#     subject { book.valid? }
-    
-#     let(:user) { create(:user) }
-#     let!(:book) { build(:book, user_id: user.id) }
-    
-#     context 'titleカラム' do
-#       it '空欄ではないこと' do
-#         book.title = ''
-#         is_expected.to eq false
-#       end
-#     end
-    
-#     context 'bodyカラム' do
-#       it '空欄ではないこと' do
-#         book.body = ''
-#         is_expected.to eq false
-#       end
-#       it '200文字以下であること: 200文字はOK' do
-#         book.body = Faker::Lorem.characters(number: 200)
-#         is_expected.to eq true
-#       end
-#       it '200文字以下であること: 201文字はNG' do
-#         book.body = Faker::Lorem.characters(number: 201)
-#         is_expected.to eq false
-#       end
+# RSpec.describe Book, "モデルに関するテスト", type: :model do
+#   describe '実際に保存してみる' do
+#     it "有効な投稿内容の場合は保存されるか" do
+#       expect(FactoryBot.build(:book)).to be_valid
 #     end
 #   end
-  
-#   describe 'アソシエーションのテスト' do
-#     context 'Userモデルとの関係' do
-#       it 'N:1となっている' do
-#         expect(Book.reflect_on_assosiation(:user).macro).to eq :belongs_to
-#       end
+#   context "空白のバリデーションチェック" do
+#     it "titleが空白の場合にバリデーションチェックされ空白のエラーメッセージがが返ってきているか" do
+#       book = Book.new(title: '', body: 'hoge')
+#       expect(book).to be_invalid
+#       expect(book.errors[:title]).to include("can't be blank")
+#     end
+#     it "bodyが空白の場合にバリデーションチェックされ空白のエラーメッセージがが返ってきているか" do
+#       book = Book.new(title: 'hoge', body: '')
+#       expect(book).to be_invalid
+#       expect(book.errors[:body]).to include("can't be blank")
 #     end
 #   end
 # end
+
+RSpec.describe 'Bookモデルのテスト', type: :model do
+  describe 'バリデーションのテスト' do
+    subject { book.valid? }
+    
+    let(:user) { create(:user) }
+    let!(:book) { build(:book, user_id: user.id) }
+    
+    context 'titleカラム' do
+      it '空欄ではないこと' do
+        book.title = ''
+        is_expected.to eq false
+      end
+    end
+    
+    context 'bodyカラム' do
+      it '空欄ではないこと' do
+        book.body = ''
+        is_expected.to eq false
+      end
+      it '200文字以下であること: 200文字はOK' do
+        book.body = Faker::Lorem.characters(number: 200)
+        is_expected.to eq true
+      end
+      it '200文字以下であること: 201文字はNG' do
+        book.body = Faker::Lorem.characters(number: 201)
+        is_expected.to eq false
+      end
+    end
+  end
+  
+  describe 'アソシエーションのテスト' do
+    context 'Userモデルとの関係' do
+      it 'N:1となっている' do
+        expect(Book.reflect_on_association(:user).macro).to eq :belongs_to
+      end
+    end
+  end
+end
