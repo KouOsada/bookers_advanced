72c8e27394fb4935c05751edc0f2f366bb157957
KouOsada <kouosada.0930@gmail.com>
2022-01-11 12:58:26 +0000

    [Add]book_spec.rb

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -21,3 +21,42 @@ RSpec.describe Book, "モデルに関するテスト", type: :model do
     end
   end
 end
+
+# Rspec.describe 'Bookモデルのテスト', type: :model do
+#   describe 'バリデーションのテスト' do
+#     subject { book.valid? }
+    
+#     let(:user) { create(:user) }
+#     let!(:book) { build(:book, user_id: user.id) }
+    
+#     context 'titleカラム' do
+#       it '空欄ではないこと' do
+#         book.title = ''
+#         is_expected.to eq false
+#       end
+#     end
+    
+#     context 'bodyカラム' do
+#       it '空欄ではないこと' do
+#         book.body = ''
+#         is_expected.to eq false
+#       end
+#       it '200文字以下であること: 200文字はOK' do
+#         book.body = Faker::Lorem.characters(number: 200)
+#         is_expected.to eq true
+#       end
+#       it '200文字以下であること: 201文字はNG' do
+#         book.body = Faker::Lorem.characters(number: 201)
+#         is_expected.to eq false
+#       end
+#     end
+#   end
+  
+#   describe 'アソシエーションのテスト' do
+#     context 'Userモデルとの関係' do
+#       it 'N:1となっている' do
+#         expect(Book.reflect_on_assosiation(:user).macro).to eq :belongs_to
+#       end
+#     end
+#   end
+# end
